ff9698f4581bb65ea22d8f46618fd1b8505bf3ad
bzxnaga <bzxnaga@gmail.com>
2020-02-13 20:55:24 +0700

    20:55

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
-var datapembelian = {};
-var memberId = "1adsfaewrdf"
-var money = 50000
-datapembelian.memberId = memberId;
-datapembelian.money = money;
-datapembelian.listPurchased =[];
+// var datapembelian = {};
+// var memberId = "1adsfaewrdf"
+// var money = 50000
+// datapembelian.memberId = memberId;
+// datapembelian.money = money;
+// datapembelian.listPurchased =[];
 
-console.log (datapembelian)
+// console.log (datapembelian)
+
+let listBarang = [ ['Sepatu Stacattu', 1500000, 10],
+                       ['Baju Zoro', 500000, 2],
+                       ['Sweater Uniklooh', 175000, 1]
+                     ];
+  
+    // you can only write your code here!
+    var shoppers =[]
+console.log(listBarang[0][0])
+console.log(listBarang[0][1])
+console.log(listBarang[0][2])
